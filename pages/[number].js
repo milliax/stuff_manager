e85a1730ff1fa494a4a78d2694813b330efc89f0
milliax <wei@sivir.pw>
2022-11-16 21:44:40 +0800

    temp

--- pages/[number].js
+++ pages/[number].js
@@ -2,33 +2,79 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMinus, faPencil, faPlus } from "@fortawesome/free-solid-svg-icons"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
-import {Swal} from "sweetalert2"
+import { Swal } from "sweetalert2"
 
 export default function AvailableStuff({ props }) {
-    const [stuff, setStuff] = useState({
-        "八寶粥": 8,
-        "好吃的飯": 1
-    })
+    const [loading, setLoading] = useState(true)
+    const [stuff, setStuff] = useState({})
     const router = useRouter()
-    // console.log(props)
 
     const toggleAmountChange = (index, delta) => {
         // console.log(parseInt(stuff[index]) + delta)
         const newNumber = parseInt(stuff[index]) + delta
-        setStuff({
-            ...stuff,
-            [index]: newNumber
-        })
+
+        if (newNumber === 0) {
+            Swal.fire({
+                title: `${index}吃/用完了嗎 ?`,
+                icon: "question"
+            }).then(result => {
+                if (result.isConfirmed) {
+                    // TODO: delete item
+                }
+            })
+        } else {
+            setStuff({
+                ...stuff,
+                [index]: newNumber
+            })
+        }
     }
 
     const editProductName = async (index) => {
- 
+        result = await Swal.fire({
+            title: "輸入要修改成的名稱",
+            text: `原本名稱：${index}`,
+            inputAttributes: {
+                autocapitalize: "off"
+            },
+            showLoaderOnConfirm: true,
+            preConfirm: (newName) => {
+                return fetch(`/api/delete`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        room: props.token,
+                        item: index
+                    })
+                }).then(response => {
+                    if (!response.ok) {
+                        throw new Error(response.statusText)
+                    }
+                    return response.json()
+                }).catch(error => {
+                    Swal.showValidatationMessage(`Request failed: ${error}`)
+                })
+            },
+            allowOutsideClick: () => !Swal.isLoading()
+        })
+
+        if (result.isConfirmed) {
+            return Swal.fire({
+                title: `刪除${index}成功`,
+                icon: "success"
+            })
+        }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         //TODO: fetch the latest stuff list
-
-    },[])
+        setStuff({
+            "八寶粥": 8,
+            "好吃的飯": 1
+        })
+        // setTimeout(() => {
+        //     setLoading(false)
+        // }, 1000)
+    }, [])
 
     if (props.exist) {
         return (
@@ -38,33 +84,42 @@ export default function AvailableStuff({ props }) {
 
                 <div className="pt-5 flex flex-row justify-center">
                     <div className="w-1/2 flex flex-col space-y-3">
-                        {Object.keys(stuff).map(index => (
-                            <div className="bg-blue-100 rounded-full px-5 py-2 flex flex-row justify-between" key={index}>
-                                <div className="flex flex-row space-x-2">
-                                    <div>{index}</div>
-                                    <button onClick={() => {
-                                        editProductName(index);
-                                    }}>
-                                        <FontAwesomeIcon icon={faPencil} />
-                                    </button>
+                        {loading ?
+                            Object.keys(Array(3)).map((item, cnt) => (
+                                <div className="bg-blue-100 rounded-full px-5 py-2 grid grid-cols-5 gap-3 animate-pulse" key={cnt}>
+                                    <div className="h-2 bg-slate-200 col-span-3"/>
+                                    <div className="h-2 bg-slate-200 col-span-2"/>
                                 </div>
-                                <div className="flex flex-row items-center space-x-2">
-                                    <button className="bg-pink-300 rounded-lg aspect-square w-6"
-                                        onClick={() => {
-                                            toggleAmountChange(index, -1)
+                            ))
+                            :
+                            Object.keys(stuff).map(index => (
+                                <div className="bg-blue-100 rounded-full px-5 py-2 flex flex-row justify-between" key={index}>
+                                    <div className="flex flex-row space-x-2">
+                                        <div>{index}</div>
+                                        <button onClick={() => {
+                                            editProductName(index);
                                         }}>
-                                        <FontAwesomeIcon icon={faMinus} />
-                                    </button>
-                                    <div className="bg-neutral-50 px-1">{stuff[index]}</div>
-                                    <button className="bg-red-300 rounded-lg aspect-square w-6"
-                                        onClick={() => {
-                                            toggleAmountChange(index, 1)
-                                        }}>
-                                        <FontAwesomeIcon icon={faPlus} />
-                                    </button>
+                                            <FontAwesomeIcon icon={faPencil} />
+                                        </button>
+                                    </div>
+                                    <div className="flex flex-row items-center space-x-2">
+                                        <button className="bg-pink-300 rounded-lg aspect-square w-6"
+                                            onClick={() => {
+                                                toggleAmountChange(index, -1)
+                                            }}>
+                                            <FontAwesomeIcon icon={faMinus} />
+                                        </button>
+                                        <div className="bg-neutral-50 px-1">{stuff[index]}</div>
+                                        <button className="bg-red-300 rounded-lg aspect-square w-6"
+                                            onClick={() => {
+                                                toggleAmountChange(index, 1)
+                                            }}>
+                                            <FontAwesomeIcon icon={faPlus} />
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
+
                         <div>
                             New Item
                         </div>
@@ -91,6 +146,7 @@ export async function getServerSideProps(context) {
         2. add account system
      */
     // TODO: No websocket support yet
+    // TODO: generate a token and send it to frontend to retrieve the item data?
     const token = context.params.number
     return {
         props: {
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import { useRef } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faSearch } from "@fortawesome/free-solid-svg-icons"
 import Link from "next/link"
+import Swal from "sweetalert2"
 
 export default function Home() {
     const { inputRef } = useRef()
 
     const searchRooms = () => {
-
+        Swal.fire({
+            icon: "info",
+            title: "Function Not Implemented Yet !!!"
+        })
     }
 
     return (
@@ -31,14 +35,13 @@ export default function Home() {
                     </form>
                 </div>
                 <div className="pt-10 flex flex-row justify-center">
-                    <div className="w-1/2">
+                    <div className="md:w-1/2 w-4/5">
                         <div className="text-xl">快速房間連結</div>
 
-                        <div className="py-5 grid grid-cols-3 gap-5">
+                        <div className="py-5 grid md:grid-cols-3 grid-cols-1 gap-5">
                         {[{ dorm: "12", room: "619" }, { dorm: "10", room: "308" }, { dorm: "10", room: "319" }].map(bundle => (
-                                <Link href={`${bundle.dorm}${bundle.room}`} passHref>
-                                    <div key={`${bundle.dorm}${bundle.room}`}
-                                        className="bg-blue-100 py-2 px-2 rounded-3xl flex flex-row w-36">
+                                <Link href={`${bundle.dorm}${bundle.room}`} key={`${bundle.dorm}${bundle.room}`} passHref>
+                                    <div className="bg-blue-100 py-2 px-2 rounded-3xl flex flex-row w-36">
                                         <div className="px-5">
                                             {`${bundle.dorm}舍`}
                                         </div>
